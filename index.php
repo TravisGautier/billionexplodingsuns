f1aca4e990253b42ce507b62b2d06c1cb5406cb6
Travis Gautier <tgautier13@gmail.com>
2019-10-18 12:35:06 -0500

    added the_title() to have_posts() loop

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php if (have_posts()) : ?>
     <?php while ( have_posts()) : the_post(); ?>
+        <?php the_title(); ?>
         <?php the_content(); ?>
     <?php endwhile; ?>
 <?php endif; ?>
